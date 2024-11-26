186f664a8495d0c1f912893a642041feca79a477
224654 <224654@live.brock.ac.uk>
2024-11-26 14:40:05 +0000

    Loads homepaige

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnIn = new System.Windows.Forms.Button();
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnGuest = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnIn
+            // 
+            this.btnIn.Location = new System.Drawing.Point(72, 170);
+            this.btnIn.Name = "btnIn";
+            this.btnIn.Size = new System.Drawing.Size(178, 70);
+            this.btnIn.TabIndex = 0;
+            this.btnIn.Text = "Sign in";
+            this.btnIn.UseVisualStyleBackColor = true;
+            // 
+            // btnUp
+            // 
+            this.btnUp.Location = new System.Drawing.Point(265, 170);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(178, 70);
+            this.btnUp.TabIndex = 1;
+            this.btnUp.Text = "Sign up";
+            this.btnUp.UseVisualStyleBackColor = true;
+            // 
+            // btnGuest
+            // 
+            this.btnGuest.Location = new System.Drawing.Point(72, 324);
+            this.btnGuest.Name = "btnGuest";
+            this.btnGuest.Size = new System.Drawing.Size(178, 70);
+            this.btnGuest.TabIndex = 2;
+            this.btnGuest.Text = "Log in as a guest";
+            this.btnGuest.UseVisualStyleBackColor = true;
+            this.btnGuest.Click += new System.EventHandler(this.btnGuest_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGuest);
+            this.Controls.Add(this.btnUp);
+            this.Controls.Add(this.btnIn);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnIn;
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnGuest;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
 
 namespace NoaHunterNEA
 {
@@ -15,6 +16,41 @@ namespace NoaHunterNEA
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void lstUsers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        /*public void DisplayData()
+        {
+            clsDBConnector dbConnector = new clsDBConnector();
+            OleDbDataReader dr;
+            string sqlStr;
+            dbConnector.Connect();
+            sqlStr = "SELECT Fname, Sname, Email, DOB FROM tblUsers";
+            dr = dbConnector.DoSQL(sqlStr);
+            lstUsers.Items.Clear();
+            while (dr.Read())
+            {
+                lstUsers.Items.Add(dr[0].ToString());
+                lstUsers.Items[lstUsers.Items.Count - 1].SubItems.Add(dr[1].ToString());
+                lstUsers.Items[lstUsers.Items.Count - 1].SubItems.Add(dr[2].ToString());
+                lstUsers.Items[lstUsers.Items.Count - 1].SubItems.Add(dr[3].ToString());
+            }
+            dbConnector.Close();
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            DisplayData();
+        }
+        */
+        private void btnGuest_Click(object sender, EventArgs e)
+        {
+            HomePage homePage = new HomePage();
+            homePage.ShowDialog(); //dialog stops being able to use form below 
         }
     }
 }
